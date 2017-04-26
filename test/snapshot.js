2aab289870ce202467d3c8fd8bb8e40a2560cbf5
Dmitry Tsvettsikh <dmitrycvet@gmail.com>
2017-04-26 19:26:28 +0500

    add tests

--- test/snapshot.js
+++ test/snapshot.js
@@ -19,10 +19,20 @@ test('one field', async t => {
   t.deepEqual(Object.keys(tasks[0]), ['pid'])
 })
 
-test('multiple fields', async t => {
+test('multiple fields as arguments', async t => {
   const tasks = await ps.snapshot('pid', 'name')
 
   t.true(Array.isArray(tasks))
   t.not(tasks.length, 0)
   t.deepEqual(Object.keys(tasks[0]), ['name', 'pid'])
+})
+
+test('check every field one-by-one', async t => {
+  for (const field of ps.allowedFields) {
+    const tasks = await ps.snapshot(field)
+
+    t.true(Array.isArray(tasks))
+    t.not(tasks.length, 0)
+    t.deepEqual(Object.keys(tasks[0]), [field])
+  }
 })
